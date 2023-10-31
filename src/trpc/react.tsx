7dd37120d4c14f3d01d8aa4ceba2eea2cd8be313
AlandSleman <alandsazkm@gmail.com>
2023-10-31 14:57:23 +0300

    add shadcn and organize

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -6,7 +6,7 @@ import { createTRPCReact } from "@trpc/react-query";
 import { useState } from "react";
 
 import { type AppRouter } from "@/server/api/root";
-import { getUrl, transformer } from "./shared";
+import { getUrl, transformer } from "@/trpc/shared";
 
 export const api = createTRPCReact<AppRouter>();
 
@@ -34,7 +34,7 @@ export function TRPCReactProvider(props: {
           },
         }),
       ],
-    })
+    }),
   );
 
   return (
